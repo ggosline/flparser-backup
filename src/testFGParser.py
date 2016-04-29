c8dff5e2eaa84df3387c7849325ff6fe9b7abf4b
George Gosline <g.gosline@kew.org>
2016-04-29 16:01:41 +0100

    use appliesto in parsing

--- src/testFGParser.py
+++ src/testFGParser.py
@@ -19,13 +19,13 @@ logging.basicConfig(filename='flparse.log', filemode='w', level=logging.INFO)
 description = 'fruit many-seeded'
 # description = 'anthers 3, pale to orange-yellow, dehiscing by 2 oblique or almost vertical clefts not confluent at the apex'
 # query="Select * from AllTaxa where flora_name = 'FZ' and rank = 'species' and genus = 'Salacia' ;"
-# query="Select * from AllTaxa where flora_name = 'FZ' and rank = 'species' and genus = 'Salacia' and species = 'bussei' ;"
+query="Select * from AllTaxa where flora_name = 'FTEA' and rank = 'species' and genus = 'Salacia' and species = 'erecta' ;"
 # query="Select * from AllTaxa where flora_name = 'FZ' and rank = 'species' and genus = 'Acacia' and species = 'albida'  ;"
 # query="Select * from AllTaxa where flora_name = 'FTEA' and rank = 'species' and genus = 'Allophylus' and species = 'abyssinicus' ;"
-query="Select * from AllTaxa where flora_name = 'FZ' and rank = 'genus' and family = 'Annonaceae' ;"
+# query="Select * from AllTaxa where flora_name = 'FZ' and rank = 'genus' and genus = 'Oxalis' ;"
 
 fromDB = True
-fromDB = False
+#fromDB = False
 
 draw = False
 draw = True
