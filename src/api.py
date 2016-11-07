2fdf32a796cb717b13c18546709c339946c6dfb3
George Gosline <g.gosline@kew.org>
2016-11-07 12:44:56 +0000

    eflora Database query sqlalchemy

--- src/api.py
+++ src/api.py
@@ -5,6 +5,7 @@ import site
 site.addsitedir('src')
 
 from floraparser.TaxaToCharacters import flDescToCharacters, flDescToPhrases
+from floracorpus.efloraQuery import readEflora
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,42 +15,53 @@ wsgi_app = app.wsgi_app
 #     if todo_id not in TODOS:
 #         abort(404, message="Todo {} doesn't exist".format(todo_id))
 
-parser = reqparse.RequestParser()
-parser.add_argument('taxonNo')
-parser.add_argument('family')
-parser.add_argument('genus', required=True)
-parser.add_argument('species', required=True)
-parser.add_argument('rank')
-parser.add_argument('description', required=True)
+flpparser = reqparse.RequestParser()
+flpparser.add_argument('taxonNo')
+flpparser.add_argument('family')
+flpparser.add_argument('genus', required=True)
+flpparser.add_argument('species', required=True)
+flpparser.add_argument('rank')
+flpparser.add_argument('description', required=True)
 
+dbparser = reqparse.RequestParser()
+dbparser.add_argument('flora')
+dbparser.add_argument('rank')
+dbparser.add_argument('genus')
+dbparser.add_argument('species')
 
 # pass parameters including description to flparser
 # returns a JSON list of characters
 class descParse(Resource):
 
     def get(self):
-        args = parser.parse_args()
+        args = flpparser.parse_args()
         return flDescToCharacters(ttrace=0, **args)
 
     def post(self):
-        args = parser.parse_args()
+        args = flpparser.parse_args()
         return flDescToCharacters(ttrace=0, **args)
 
 class descParsePhrases(Resource):
 
     def get(self):
-        args = parser.parse_args()
+        args = flpparser.parse_args()
         return flDescToPhrases(ttrace=0, **args)
 
     def post(self):
-        args = parser.parse_args()
+        args = flpparser.parse_args()
         return flDescToPhrases(ttrace=0, **args)
+
+class efloraDescriptions(Resource):
+
+    def get(self):
+        return readEflora(dbparser.parse_args())
 
 ##
 ## Actually setup the Api resource routing here
 ##
 api.add_resource(descParse, '/characters')
 api.add_resource(descParsePhrases, '/phrases')
+api.add_resource(efloraDescriptions, '/eflora')
 
 if __name__ == '__main__':
     app.run()
--- src/floracorpus/efloraQuery.py
+++ src/floracorpus/efloraQuery.py
@@ -1,10 +1,26 @@
-from sqlalchemy import create_engine
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
 
-e = create_engine(r'sqlite:///..\resources\efloras.db3')
+db = create_engine(r'sqlite:///..\resources\efloras.db3')
+Session = sessionmaker(bind=db)
+session = Session()
+db.echo = False  # Try changing this to True and see what happens
+metadata = MetaData()
+taxa = Table('alltaxa', metadata, autoload=True, autoload_with=db)
+
+def run(stmt):
+    rs = db.execute(stmt)
+    return [dict(zip(row.keys(), row)) for row in rs]
+
+def readEflora(searchargs):
+        q = session.query(taxa).filter(taxa.c.flora_name == 'FZ').filter(taxa.c.genus == 'Diospyros')
+        return [row for row in q]
+        # s = taxa.select(and_(taxa.c.flora_name == 'FZ', taxa.c.genus == 'Diospyros') )
+        # return run(s)
 
 #Connect to databse
 if __name__ == '__main__':
-        conn = e.connect()
+        conn = db.connect()
         #Perform query and return JSON data
         query = conn.execute("select flora_code from Floradetails order by flora_code")
         print ({'floras': [i for i in query.cursor.fetchall()]})
